ca545a159cf789631ec58cd8f294b945ee5ee910
David <dsanzraya@hotmail.com>
2024-09-20 11:53:40 +0200

    triangle perimeter

--- Shapes/Triangle.cs
+++ Shapes/Triangle.cs
@@ -1,9 +1,12 @@
 ﻿
+using System;
+
 namespace Shapes
 {
     internal class Triangle : Polygon
     {
         private double b, height, alpha;
+        private double side1, side2, side3;
 
         public Triangle(double b, double height, double alpha)
         {
@@ -11,6 +14,7 @@ namespace Shapes
             this.b = b;
             this.height = height;
             this.alpha = alpha;
+            CalculateSides();
         }
 
         public override double Area()
@@ -20,7 +24,14 @@ namespace Shapes
 
         public override double Perimeter()
         {
-            return base.Perimeter();
+            return side1 + side2 + side3;
+        }
+
+        private void CalculateSides()
+        {
+            side1 = b;
+            side2 = Math.Sqrt(Math.Pow(alpha, 2) + Math.Pow(height, 2));
+            side3 = Math.Sqrt(Math.Pow(b - alpha, 2) + Math.Pow(height, 2));
         }
     }
 }
