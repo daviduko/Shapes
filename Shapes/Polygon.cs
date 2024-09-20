3bf877f7a1ab40e05b25080a1bddddb039a457b9
David <dsanzraya@hotmail.com>
2024-09-20 11:09:39 +0200

    usings

--- Shapes/Polygon.cs
+++ Shapes/Polygon.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Shapes
 {
     internal class Polygon : Shape2D
--- Shapes/Rectangle.cs
+++ Shapes/Rectangle.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Shapes
 {
     internal class Rectangle : Polygon
